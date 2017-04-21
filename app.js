c28e09dc909098c1073c22e250db822a11bf7554
Mathieu PATUREL <australie.p@gmail.com>
2017-04-21 16:26:39 +1000

    :tada: config manager working

---
+++ app.js
@@ -0,0 +1,90 @@
+"use strict";
+
+class ConfigManager {
+
+    // get and set the scripts and styles
+
+    static init() {
+        this.storage = chrome.storage.local
+        this.keyOfPatternList = '.___urls'
+    }
+
+    static matches(pattern, url) {
+        return new RegExp(pattern).test(url)
+    }
+
+    static getFromStorage(keys) {
+        return new Promise((resolve, reject) => {
+            this.storage.get(keys, obj => {
+                resolve(obj)
+            })
+        })
+    }
+
+    static getFor(url) {
+        // return a list of configurations
+        return this.getFromStorage(this.keyOfPatternList)
+            .then(object => {
+                const patterns = object[this.keyOfPatternList]
+                const matches = []
+
+                patterns.some((pattern) => {
+                    if (this.matches(pattern, url)) {
+                        matches.push(pattern)
+                    }
+                })
+                return matches
+            }).then(matches => {
+                const promises = []
+                matches.some((pattern) => {
+                    promises.push(this.getFromStorage(pattern))
+                })
+                return Promise.all(promises)
+            }).then(configs => {
+                const cleanConfigs = {}
+                configs.map(config => {
+                    const key = Object.keys(config)[0]
+                    cleanConfigs[key] = config[key]
+                })
+                return cleanConfigs
+            })
+    }
+
+    static setFor(pattern, config) {
+        return new Promise((resolve, reject) => {
+            this.storage.get(this.keyOfPatternList, (object) => {
+                let urls;
+                if (object[this.keyOfPatternList] === undefined) {
+                    urls = []
+                } else {
+                    urls = object[this.keyOfPatternList]
+                }
+                if (urls.indexOf(pattern) === -1) {
+                    urls.push(pattern)
+                    let obj = {}
+                    obj[this.keyOfPatternList] = urls
+                    this.storage.set(obj)
+                }
+                let obj = {}
+                obj[pattern] = config
+                this.storage.set(obj, () => {
+                    resolve()
+                })
+            })
+        })
+    }
+
+}
+
+// chrome.browserAction.onClicked.addListener(_ => {
+    ConfigManager.init()
+    ConfigManager.setFor('google\\.com', {
+        css: "body { color: red }"
+    }).then(_ => ConfigManager.setFor('google\\.com\\.au', {
+        css: "body { font-style: italic; }"
+    })).then(_ => {
+        return ConfigManager.getFor('https://google.com.au')
+    }).then(configs => {
+        console.log(configs)
+    })
+// })
