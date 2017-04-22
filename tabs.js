a394e07e8151f8c4168684e493a13b8c7037525c
Mathieu PATUREL <australie.p@gmail.com>
2017-04-23 08:09:00 +1000

    :sparkles: use hash in url for tabs

--- tabs.js
+++ tabs.js
@@ -7,6 +7,7 @@ class Tabs {
         this.container = container
 
         this.cacheDOM()
+        this.showTabFromHash()
         this.bindDOM()
 
     }
@@ -26,24 +27,41 @@ class Tabs {
         this.underline.style.left = element.offsetLeft + 'px'
     }
 
+    showTabFromHash() {
+        const tabName = new URLSearchParams(location.hash.slice(1)).get('tab')
+        if (tabName === null) {
+            return
+        }
+
+        const tab = this.tabs.querySelector(`.tab[data-tab=${tabName}]`)
+        if (tab === null) {
+            throw new Error(`Couldn't find tab for ${tabName}`)
+        }
+        const tabContent = this.tabsContent.querySelector(`#tab-${tabName}`)
+        if (tabContent === null) {
+            throw new Error(`Couldn't find tab content for '${tabName}'`)
+        }
+        this.currentTab.classList.remove('active')
+        tab.classList.add('active')
+        this.currentTab = tab
+        this.moveUnderlineTo(this.currentTab)
+
+        this.currentTabContent.classList.remove('active')
+        tabContent.classList.add('active')
+        this.currentTabContent = tabContent
+    }
+
     bindDOM() {
+        window.addEventListener('hashchange', this.showTabFromHash.bind(this))
+
         this.tabs.addEventListener('click', e => {
             const tabName = e.target.getAttribute('data-tab')
             if (tabName === null) {
                 return
             }
-            const tabContent = this.tabsContent.querySelector(`#tab-${tabName}`)
-            if (tabContent === null) {
-                throw new Error(`Couldn't find tab with the for '${tabName}'`)
-            }
-            this.currentTab.classList.remove('active')
-            e.target.classList.add('active')
-            this.currentTab = e.target
-            this.moveUnderlineTo(this.currentTab)
-
-            this.currentTabContent.classList.remove('active')
-            tabContent.classList.add('active')
-            this.currentTabContent = tabContent
+            let hash = new URLSearchParams(location.hash.slice(1))
+            hash.set('tab', tabName)
+            location.hash = hash.toString()
         })
     }
 
